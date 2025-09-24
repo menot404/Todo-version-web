a77f62074bb796d3d7e6eedb2da0c03208ff2f8d
Me_Not404 <m3not404@gmail.com>
2025-09-24 00:14:42 +0000

    feat: add feat dark-mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import HomeTodo from "./components/HomeTodo"
 import Interface from "./components/common/Interface"
 import useTimeLoading from "./logic/timeLoading"
+import { ThemeProvider } from "./components/Theme-provider"
+
 function App() {
 
   return (
     <>
       {
-       useTimeLoading() ? <Interface/> : <HomeTodo />
+        useTimeLoading() ? <Interface/> 
+          :
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <HomeTodo />
+      </ThemeProvider>
+        
       }
     </>
   )
