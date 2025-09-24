28987bdfc718c46f03f9677433ba1cb3dcb3005b
Me_Not404 <m3not404@gmail.com>
2025-09-24 00:14:42 +0000

    feat: add feat dark-mode

--- src/components/form/SearchTask.tsx
+++ src/components/form/SearchTask.tsx
@@ -1,6 +1,6 @@
 import { Input } from "@/components/ui/input"
 import { Search } from "lucide-react"
-
+import { ModeToggle } from "../Mode-toggle";
 interface SearchTaskProps {
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -8,15 +8,19 @@ interface SearchTaskProps {
 
 const SearchTask = ({ value, onChange }: SearchTaskProps) => {
   return (
-    <div className="relative w-full max-w-md mx-auto">
-      <Input
-        type="text"
-        value={value}
-        onChange={onChange}
-        placeholder="Rechercher une tâche..."
-        className="pr-10 text-base"
-      />
-      <Search className="absolute right-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400 pointer-events-none" />
+    <div>
+      <div className="relative w-full max-w-md mx-auto">
+        <Input
+          type="text"
+          value={value}
+          onChange={onChange}
+          placeholder="Rechercher une tâche..."
+          className="pr-10 text-base"
+        />
+        <Search className="absolute right-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400 pointer-events-none" />
+        <ModeToggle/>
+      </div>
+      
     </div>
   )
 }
